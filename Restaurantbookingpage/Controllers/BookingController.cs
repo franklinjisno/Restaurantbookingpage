dce394d7c05e73dba5398ddf585875b275a0e7f5
Meenu.Prasannakumar <meenu.prasannakumar@ust.com>
2022-03-02 09:30:36 +0530

    02/03/2022 - 9:30

--- Restaurantbookingpage/Controllers/BookingController.cs
+++ Restaurantbookingpage/Controllers/BookingController.cs
@@ -150,7 +150,7 @@ namespace Restaurantbookingpage.Controllers
             Bookingdbhandle objdbhandle = new Bookingdbhandle();
             List<Booking> objbooking = objdbhandle.ListOfBooking();
 
-            var table = new DataTable("Example Table");
+            var table = new DataTable("Booking Report");
             table.Columns.Add("Customer Name", typeof(string));
             table.Columns.Add("DateTime", typeof(DateTime));
             table.Columns.Add("DiningType", typeof(string));
@@ -165,44 +165,7 @@ namespace Restaurantbookingpage.Controllers
             System.IO.File.WriteAllBytes(@"D:\Project-Franklin\Restaurantbookingpage\PDF\result", pdf);
 
             return PartialView("_PrintView");
-
-            //Encoding u8 = Encoding.UTF8;
-            //byte[] result = objbooking.SelectMany(x => u8.GetBytes(x.Customer_Name + "   " + x.Category + "\n")).ToArray();
-            //byte[] data = Encoding.ASCII.GetBytes(objbooking.ToString()).ToArray();
-            //byte[] data = new byte[100] ;
-            //var pdf = objbooking.ToPdf(schema => schema.ColumnName(m => m. != "Prop2"));
-            //byte[] pdf = objbooking.ToPdf();
-           
-            //foreach (Booking booking in objbooking) {
-            //   //data.Add(u8.GetBytes(booking.Customer_Name));
-            //   data[0] = u8.GetAllBytes(booking.Customer_Name);
-            //               }
-            // string s = u8.GetString(result);
-            //System.IO.File.ReadAllBytes(@"Downloads")
-            //System.IO.File.WriteAllBytes(@"F:\testpdf.pdf", result);
-           
-
-
-            //string s = u8.GetString(result);
-            //byte[] bytes = objbooking.SelectMany(x => x).SelectMany(BitConverter.GetBytes).ToArray();
-            //var binFormatter = new BinaryFormatter();
-            //var mStream = new MemoryStream();
-            //binFormatter.Serialize(mStream, objbooking);
-            //mStream.ToArray();
-            //return new FileStreamResult(mStream, "application/pdf");
-
-
-            //var report = new ActionAsPdf(objdbhandle.ListOfBooking);
-            //return PartialView("_PrintView",report);
-
-
-            //byte[] bytes;
-            //BinaryFormatter bf = new BinaryFormatter();
-            //MemoryStream ms = new MemoryStream();
-            //bf.Serialize(ms, objbooking);
-            //bytes = ms.ToArray();
-            //System.IO.File.WriteAllBytes(@"F:\testpdf.pdf", bytes);
-            //return PartialView("_PartialView");
+            
         }
     }
 }
